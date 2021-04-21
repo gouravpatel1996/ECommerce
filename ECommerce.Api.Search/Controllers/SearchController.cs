85bb2f483701d09c5c8200107d4ad0f72a3c0a6e
GauravProjectsRepo <82964715+GauravProjectsRepo@users.noreply.github.com>
2021-04-21 22:26:36 +0530

    Commit For Search Microservice

---
+++ ECommerce.Api.Search/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Search.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController: ControllerBase
+    {
+        private readonly ISearchService serviceProvider;
+
+        public SearchController(ISearchService serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchAsync(SearchTerm term)
+        {
+            var result = await serviceProvider.SearchAsync(term.CustomerId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.SearchResults);
+            }
+            return NotFound();
+        }
+    }
+}
