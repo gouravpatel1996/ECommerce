5a4de7fd37464588a146a89c0be26a2b7caa737e
GauravProjectsRepo <82964715+GauravProjectsRepo@users.noreply.github.com>
2021-04-21 22:33:12 +0530

    Commit for Customer Microservice

---
+++ ECommerce.Api.Customers/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using ECommerce.Api.Customers.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Controllers
+{
+    [ApiController]
+    [Route("api/customers")]
+    public class CustomersController:ControllerBase
+    {
+        private readonly ICustomersProvider customersProvider;
+
+        public CustomersController(ICustomersProvider customersProvider)
+        {
+            this.customersProvider = customersProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomersAsync()
+        {
+            var results = await customersProvider.GetCustomersAsync();
+            if (results.IsSuccess)
+            {
+                return Ok(results.Customers);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerAsync(int id)
+        {
+            var result = await customersProvider.GetCustomerAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Customer);
+            }
+            return NotFound();
+        }
+    }
+}
